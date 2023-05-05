feecea0a9bdea6935ff791c628803786520e305f
nagaraj rathna <34264078+nagaraj28@users.noreply.github.com>
2023-05-05 14:35:24 -0500

    changes

--- application.py
+++ application.py
@@ -55,4 +55,4 @@ def predictall():
     return jsonify({'predicted_close_rate': resultPredict})
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    application.run(host='0.0.0.0', port=80, debug=True)
